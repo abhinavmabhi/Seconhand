1ae14ddd1dd92eacc9892243e5b1a8a5b22f37a5
abhinavmabhi <dexmarketing192018@gmail.com>
2025-02-25 15:16:23 +0530

    Updated VehicleListView and template

--- app/views.py
+++ app/views.py
@@ -28,42 +28,42 @@ class VehicleListView(View):
         min_year_obj=VehicleModel.objects.order_by("year").first()
         max_year_obj=VehicleModel.objects.order_by("-year").first()
 
-        owner_types = Vehicle.objects.values_list('ownership_status', flat=True).distinct()
+        owner_types=Vehicle.objects.values_list('ownership_status', flat=True).distinct()
 
-        selected_ownertype = request.GET.get("owner_type", "all")
+        selected_ownertype=request.GET.get("owner_type", "all")
 
 
-        min_year = max(2000, min_year_obj.year) if min_year_obj else 2000
-        max_year = min(2025, max_year_obj.year) if max_year_obj else 2025
+        min_year=max(2000, min_year_obj.year) if min_year_obj else 2000
+        max_year=min(2025, max_year_obj.year) if max_year_obj else 2025
 
-        year_range = list(range(min_year, max_year + 1))
+        year_range=list(range(min_year, max_year + 1))
 
 
         qs = Vehicle.objects.all()
 
         # ownership 
         if selected_ownertype != "all":
-            qs = qs.filter(ownership_status=selected_ownertype)
+            qs=qs.filter(ownership_status=selected_ownertype)
 
         if selected_category !="all":
-            qs = qs.filter(category__name=selected_category)
+            qs=qs.filter(category__name=selected_category)
 
         if selected_type !="all":
-            qs = qs.filter(type__name=selected_type)
+            qs=qs.filter(type__name=selected_type)
 
         if selected_brand !="all":
-            qs = qs.filter(brand__name=selected_brand)
+            qs=qs.filter(brand__name=selected_brand)
 
         if selected_model !="all":
-            qs = qs.filter(model__year=selected_model)
+            qs=qs.filter(model__year=selected_model)
 
         # price 
 
         if min_price:
-            qs = qs.filter(price__gte=min_price)
+            qs=qs.filter(price__gte=min_price)
 
         if max_price:
-            qs = qs.filter(price__lte=max_price)
+            qs=qs.filter(price__lte=max_price)
 
         # search bar 
         if search_text:
@@ -79,14 +79,14 @@ class VehicleListView(View):
 
        
 
-        selected_model = request.GET.get("model", str(min_year))
+        selected_model=request.GET.get("model", str(min_year))
 
 
         categories=Category.objects.all()
         types=Car_or_Bike.objects.all()
         brands=Brand.objects.all()
         models=VehicleModel.objects.all()
-        last_model = VehicleModel.objects.last()
+        last_model=VehicleModel.objects.last()
 
         return render(request, 'vehicle_list.html', {
             'vehicles': qs,
@@ -107,7 +107,7 @@ class VehicleListView(View):
             "max_year": max_year,
 
             "selected_ownertype": selected_ownertype,
-            "owner_types": owner_types,  # Pass to template
+            "owner_types": owner_types, 
             
         })
     
